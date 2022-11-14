4c413cec90af7991d4990ed950cd361058494360
Victor Andres Orihuela Schanks <89089574+VictorAndresOrihuelaSchanks@users.noreply.github.com>
2022-11-13 21:50:55 -0500

    Estreno Artistas
    Anadido

--- landing-page/src/Home.jsx
+++ landing-page/src/Home.jsx
@@ -3,14 +3,17 @@ import Main from "./components/Main"
 import Guias from "./components/Guias"
 import Slider from "./components/Slider"
 import Footer from "./components/Footer"
+import Estreno from "./components/Estreno"
 
 function Home() {
   return (
     <div>
       <Main/>      
       <Slider/>
+      <Estreno/>
       <Guias/>
       <Footer/>
+
     </div>
   )
 }
