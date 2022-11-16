c0f6e34eba3df02c480cf312ad15e90a34104fab
Jose Santisteban <josesantisteban062@gmail.com>
2022-11-15 19:26:56 -0500

    About us complete

--- landing-page/src/AboutUs.jsx
+++ landing-page/src/AboutUs.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-
+import "./AboutUs.css";
+import integrante2 from "./img/rodrigo.png";
+import integrante1 from "./img/jose.jpg";
+import integrante3 from "./img/victor.png";
+import integrante4 from "./img/gonzalo.png";
+import integrante5 from "./img/cesar.png";
 function AboutUs() {
   return (
-    <div>
+    <div class="container">
       <div>
         <h1>MISION</h1>
         <p>
@@ -19,70 +24,87 @@ function AboutUs() {
       </div>
       <div>
         <h1>Integrantes</h1>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>López Takahashi, Rodrigo Andrés</Card.Title>
-            <Card.Text>
-              Como estudiante de Ciencias de la Computación de la UPC tengo
-              experiencia con Angular y Java. Las principales habilidades que
-              pongo a disposición del equipo son mis conocimientos de
-              programación para poder desarrollar el producto.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Santisteban Cerna, José Mauricio</Card.Title>
-            <Card.Text>
-              Elegí la carrera de Ciencias de la Computación por mi interés en
-              el diseño y desarrollo de videojuegos, campo en el que deseo
-              especializarme al finalizar la carrera. Asimismo, tengo
-              experiencia en el área de diseño, desarrollo y producción
-              creativa, y me gusta trabajar en equipo.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Victor Andres Orihuela Schanks</Card.Title>
-            <Card.Text>
-              Decidí estudiar ciencias de la computación porque quiero trabajar
-              haciendo videojuegos.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Jurado Garay, Gonzalo Alejandro</Card.Title>
-            <Card.Text>
-              Soy un estudiante de la carrera de Ciencias de la Computación por
-              mi interés en el cómputo y la programación, el cual ha ido
-              incrementando con el paso de los años.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Torres Paniagua, César Augusto</Card.Title>
-            <Card.Text>
-              Decidí iniciar la carrera de Ciencias de la Computación; debido a
-              que me halló interesado en los procesos automatizados con el uso
-              de AI. De esta manera, me encuentro cursando el 3 ciclo de
-              Ciencias de la Computación y me halló ansioso por adquirir las
-              habilidades para trabajar en el desarrollo de AI.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <div class="container-cartas">
+          <div class="cartas-integrantes">
+            <div class="card_title">
+              <p>José Mauricio Santisteban Cerna</p>
+            </div>
+            <div class="card_body">
+              <ul>
+                <li>Ciencias de la computación</li>
+                <li>Videojuegos</li>
+                <li>UX/UI Design</li>
+                <li>C++, Python, C#</li>
+              </ul>
+              <div class="card_image">
+                <img src={integrante1} />
+              </div>
+            </div>
+          </div>
+          <div class="cartas-integrantes">
+            <div class="card_title">
+              <p>Rodrigo Andrés López Takahashi</p>
+            </div>
+            <div class="card_body">
+              <ul>
+                <li>Ciencias de la computación</li>
+                <li>Angular Intermedio</li>
+                <li>Python, C++</li>
+              </ul>
+              <div class="card_image">
+                <img src={integrante2} />
+              </div>
+            </div>
+          </div>
+          <div class="cartas-integrantes">
+            <div class="card_title">
+              <p>Victor Andrés Orihuela Schanks</p>
+            </div>
+            <div class="card_body">
+              <ul>
+                <li>Ciencias de la computación</li>
+                <li>Videojuegos</li>
+                <li>C++</li>
+                <li>React, HTML, CSS</li>
+              </ul>
+              <div class="card_image">
+                <img src={integrante3} />
+              </div>
+            </div>
+          </div>
+          <div class="cartas-integrantes">
+            <div class="card_title">
+              <p>Gonzalo Alejandro Jurado</p>
+            </div>
+            <div class="card_body">
+              <ul>
+                <li>Ciencias de la computación</li>
+                <li>HTML, CSS</li>
+                <li>Python</li>
+                <li>C++</li>
+              </ul>
+              <div class="card_image">
+                <img src={integrante4} />
+              </div>
+            </div>
+          </div>
+          <div class="cartas-integrantes">
+            <div class="card_title">
+              <p>César Augusto Torres Paniagua</p>
+            </div>
+            <div class="card_body">
+              <ul>
+                <li>Ciencias de la computación</li>
+                <li>Inteligencia Artificial</li>
+                <li>Python</li>
+                <li>C++</li>
+              </ul>
+              <div class="card_image">
+                <img src={integrante5} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -4,13 +4,15 @@ import React from "react"
 import Header from "./components/Header"
 import Home from "./Home"
 import AboutUs from './/AboutUs';
-import {Route, Link} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/aboutus" component={AboutUs} />
+      <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/aboutus" element={<AboutUs/>} />
+      </Routes>
     </div>
   );
 }
